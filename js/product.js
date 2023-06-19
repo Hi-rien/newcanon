a66ae4016a206df13df01e38991aaf4fa0955113
Hi-rien <098sung@hanmail.net>
2023-06-19 15:01:10 +0900

    상세 디테일 수정

--- js/product.js
+++ js/product.js
@@ -2,7 +2,7 @@
 
 function inputImg() {
   for(let i = 1; i <= 7; i++) {
-    $('.slide_product').append(`<img src="../img/detail/eos-r5_0${i}.jpg"/>`)
+    $('.slide_product').append(`<img src="./img/detail/eos-r5_0${i}.jpg"/>`)
   }
 }
 
@@ -29,7 +29,7 @@ $('.option_item ul').slick({
 
 function inputImg2() {
   for(let i = 1; i <= 5; i++) {
-    $('.event_banner').append(`<img src="../img/detail/banner0${i}.jpg"/>`)
+    $('.event_banner').append(`<img src="./img/detail/banner0${i}.jpg"/>`)
   }
 }
 inputImg2()
@@ -47,8 +47,13 @@ $('.event_banner').slick({
 
 function inputImg3() {
   for(let i = 1; i <= 15; i++) {
-    $('.contents').append(`<img src="../img/detail/contents${i}.jpg"/>`)
+    $('.contents').append(`<img src="./img/detail/contents${i}.jpg"/>`)
   }
 }
 
 inputImg3()
+
+$('ul.tab li a').click(function() {
+  $('ul.tab li').removeClass('active')
+  $(this).parent().addClass('active')
+})
